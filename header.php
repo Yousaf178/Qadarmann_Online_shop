c0af3b32dc4bf7cafcef20d74a4c94435c6321ec
testdemo <youahmad224@gmail.com>
2026-07-21 18:48:42 +0500

    change in header

--- header.php
+++ header.php
@@ -173,7 +173,7 @@ session_start();
 						<!-- LOGO -->
 						<div class="col-md-3">
 							<div class="header-logo">
-								<a href="#" class="logo">
+								<a href="." class="logo">
 								<font style="font-style:normal; font-size: 23px;color: aliceblue;font-family: serif">
                                         Qadarmann Online Services
                                     </font>
